73d8fa6f33eba967991d24f0a5a15cb06cf70a57
František Kuře <zemekoulak@gmail.com>
2022-10-20 11:04:56 +0200

    Tweeks to the counting system and I/O

--- CV2/ConsoleApp/ConsoleApp.cpp
+++ CV2/ConsoleApp/ConsoleApp.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 
 
-struct {
+struct WCounter {
 	enum wordState { noWord, number, word };
 	wordState wS = noWord;
 	bool sentenceState = false;
@@ -17,70 +17,81 @@ struct {
 	int pocetVet = 0;
 	int numCount = 0;
 	int numSum = 0;
+	void count(char const ch);
 }OutputCisla;
 
 
-void count(char const ch) {
+inline void WCounter::count(char const ch) {
 
 	switch (ch)
 	{
 	case ' ':
 	case '\t':
-		if (OutputCisla.wS == OutputCisla.word) {
-			OutputCisla.pocetSlov++;
-			OutputCisla.wS = OutputCisla.noWord;
-			if (!OutputCisla.sentenceState)OutputCisla.sentenceState = true;
-			//OutputCisla.rowState = true;
+	case '\n':
+		if (wS == word) {
+			pocetSlov++;
+			wS = noWord;
+			if (!sentenceState)sentenceState = true;
+			//rowState = true;
 		}
-		else if (OutputCisla.wS == OutputCisla.number) {
-			OutputCisla.numCount++;
-			OutputCisla.numSum+=OutputCisla.currentNum;
-			OutputCisla.currentNum = 0;
-			OutputCisla.wS = OutputCisla.noWord;
-			//OutputCisla.rowState = true;
+		else if (wS == number) {
+			numCount++;
+			numSum+=currentNum;
+			currentNum = 0;
+			wS = noWord;
+			//rowState = true;
 		}
 		break;
 	case '!':
 	case '?':
 	case '.':
-		if (OutputCisla.wS == OutputCisla.word) {
-			OutputCisla.pocetSlov++;
-			OutputCisla.wS = OutputCisla.noWord;
-			if (!OutputCisla.sentenceState)OutputCisla.sentenceState = true;
-			//OutputCisla.rowState = true;
+		if (wS == word) {
+			pocetSlov++;
+			wS = noWord;
+			if (!sentenceState)sentenceState = true;
+			//rowState = true;
 		}
-		else if (OutputCisla.wS == OutputCisla.number) {
-			OutputCisla.numCount++;
-			OutputCisla.numSum += OutputCisla.currentNum;
-			OutputCisla.currentNum = 0;
-			OutputCisla.wS = OutputCisla.noWord;
-			//OutputCisla.rowState = true;
+		else if (wS == number) {
+			numCount++;
+			numSum += currentNum;
+			currentNum = 0;
+			wS = noWord;
+			//rowState = true;
 		}
-		if (OutputCisla.sentenceState) {
-			OutputCisla.pocetVet++;
-			OutputCisla.sentenceState = false;
+		if (sentenceState) {
+			pocetVet++;
+			sentenceState = false;
 		}
 		break;
 	default:
-		if (isalpha(ch))OutputCisla.wS = OutputCisla.word; 
+		if (isalpha(ch))wS = word; 
 		else if(isdigit(ch)){
-			OutputCisla.wS = OutputCisla.number;
-			OutputCisla.currentNum *= 10;
-			OutputCisla.currentNum += ch-'0';
+			wS = number;
+			currentNum *= 10;
+			currentNum += ch-'0';
 		}
-		else if (OutputCisla.wS == OutputCisla.word)OutputCisla.wS = OutputCisla.noWord;
+		else if (wS == number) { 
+			numCount++;
+			numSum += currentNum;
+			currentNum = 0;
+			wS = noWord;
+		}
 }
 }
 
 
 void ZpracovaniDat(std::string const data) {
 	//pocetZnaku
-	OutputCisla.pocetZnaku += data.length();
+	OutputCisla.pocetZnaku += (int)data.length() +1;
 	if (data.length() != 0)OutputCisla.pocetRadek++;
 	//count the rest
 	for (const char& c : data) {
-		count(c);
+		OutputCisla.count(c);
 	}
+	OutputCisla.count('\n');//to end the line
+
+
+
 	//std::for_each(data, data + charammount, count);
 
 	//sum whats summable
@@ -102,24 +113,40 @@ void ZpracovaniDat(std::string const data) {
 
 int main(int argc, char const* const* argv)
 {
-	//Pick input:
-	std::ifstream is("Lorem.txt");
-	//std::istream &is = std::cin;
-	std::string buffer;
-	while (std::getline(is, buffer)) {
-		//std::cout << buffer;//Debug purposes
-		ZpracovaniDat(buffer);
+	if (argc == 1) {
+		std::string buffer;
+		while (std::getline(std::cin, buffer)) {
+			//std::cout << buffer;
+			ZpracovaniDat(buffer);
+		}
+		/*char buffer[256];
+		while (!std::cin.eof()) {
+			std::cin.getline(buffer, 256);
+			//std::cout << buffer;//Debug purposes
+			ZpracovaniDat(buffer);
+		}*/
+
 	}
+	else {
+		for (int i = 1; i < argc; i++) {
+			std::ifstream is(argv[i]);
+			std::string buffer;
+			while (std::getline(is, buffer)) {
+				//std::cout << buffer;
+				ZpracovaniDat(buffer);
+			}
+		}
+	}
+	
 
 	std::cout
-		<< "znaku: " << OutputCisla.pocetZnaku << "\n"
-		<< "radku:" << OutputCisla.pocetRadek << "\n"
-		<< "slov:" << OutputCisla.pocetSlov << "\n"
-		<< "vet:" << OutputCisla.pocetVet << "\n"
-		<< "cisel:" << OutputCisla.numCount << "\n"
-		<< "soucet:" << OutputCisla.numSum
+		<< "znaku: " << OutputCisla.pocetZnaku -1 << "\n" //The -1 is a somewhat rude way to fix the last row not being ended by a newline character
+		<< "slov: " << OutputCisla.pocetSlov << "\n"
+		<< "vet: " << OutputCisla.pocetVet << "\n"
+		<< "radku: " << OutputCisla.pocetRadek << "\n"
+		<< "cisel: " << OutputCisla.numCount << "\n"
+		<< "soucet: " << OutputCisla.numSum
 		<< std::endl;
 
 	return 0;
 }
-
