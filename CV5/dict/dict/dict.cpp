b670afad97996f31ce9ce34c23bd97cb145c9275
František Kuře <zemekoulak@gmail.com>
2022-11-21 17:34:19 +0100

    Fixing inefficiencies and slight increase in readability.

--- CV5/dict/dict/dict.cpp
+++ CV5/dict/dict/dict.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <algorithm>
 #include <set>
+#include <stdio.h>
 
 struct comparator {
 	bool operator()(const std::string& x, const std::string& y) const
@@ -18,45 +19,47 @@ using dictionary = std::map<std::string, dictValue>;
 
 class MyMap {
 public:
-	MyMap();
-	~MyMap();
-	dictionary* innerMap;
+	/*MyMap();
+	~MyMap();*/
+	dictionary innerMap;
 	void Add(const std::string& key, const  std::string& value) {
-		if (innerMap->find(key) == innerMap->end()) {
-			(*innerMap)[key] = dictValue();
+		dictValue& Dvalue = innerMap[key];
+		if (innerMap[key].size() == 0) {
+			Dvalue = dictValue();
 		}
-		(*innerMap)[key].insert(value);
+		Dvalue.insert(value);
 	}
 	void Remove(const std::string& key, const std::string& value) {
-		for (auto i = (*innerMap)[key].begin(); i != (*innerMap)[key].end(); i++) {
-			if (*i == value) {
-				(*innerMap)[key].erase(i);
-				break;
-			}
+		dictValue& Dvalue = innerMap[key];
+		auto i = Dvalue.find(value);
+		if(i!= Dvalue.end())
+			Dvalue.erase(i);
 
-		}
-		if ((*innerMap)[key].size() == 0)(*innerMap).erase(key);
+		if (Dvalue.empty())innerMap.erase(key);
 	}
 	void Remove(const std::string& key) {
-		(*innerMap).erase(key);
+		innerMap.erase(key);
 	}
 	const dictValue& Find(const std::string& key) {
-		return (*innerMap)[key];
+		return innerMap[key];
 	}
 	const dictionary::iterator FindAt(const std::string& key) {
-		return (*innerMap).find(key);
+		return innerMap.find(key);
 	}
-
+	const size_t Count(const std::string& key) {
+		return innerMap.count(key);
+	}
 };
-MyMap::MyMap()
+/*MyMap::MyMap()
 {
-	innerMap = new dictionary();
+	innerMap = dictionary();
 }
 
 MyMap::~MyMap()
 {
 	delete innerMap;
 }
+*/
 bool Isprefix(const std::string& key, const std::string& prefix) {
 	if (key.size() < prefix.size()) return false;
 	for (unsigned short c = 0; c < prefix.size(); c++)//Hehe c++
@@ -67,12 +70,15 @@ bool Isprefix(const std::string& key, const std::string& prefix) {
 	}
 	return true;
 }
-void ParseCommand(MyMap& dict, const std::string& buffer) {
+void ParseCommand(MyMap& dict, const std::string& command, const std::string& key, const std::string& value) {
+	//std::cout << command<<std::endl;
+	//std::cout << key << std::endl;
+	//std::cout << value << std::endl;
+	/*
 	if (buffer.length() == 0)return;
 	unsigned short i = 0;
 	while ((i < buffer.length() - 1) && (buffer[i] != ' ')) i++;
 	std::string command = buffer.substr(0, i);
-	transform(command.begin(), command.end(), command.begin(), ::tolower);
 	i++;
 	if (i >= buffer.length())return;
 	unsigned short j = i + 1;
@@ -80,7 +86,6 @@ void ParseCommand(MyMap& dict, const std::string& buffer) {
 		j++;
 	}
 	std::string key = buffer.substr(i, j - i);
-	transform(key.begin(), key.end(), key.begin(), ::tolower);
 	std::string value;
 	if (j < buffer.length()) {
 		j++;
@@ -88,19 +93,21 @@ void ParseCommand(MyMap& dict, const std::string& buffer) {
 		while ((k < buffer.length()) && (buffer[k] != ' ')) k++;
 		value = buffer.substr(j, k - j);
 	}
-	else value = "";
-	transform(value.begin(), value.end(), value.begin(), ::tolower);
+	else value = "";*/
+	
 	if (command == "add") {
 		dict.Add(key, value);
-		//std::cout<< "Added succesfully"<<std::endl
+		//std::cout << "Added succesfully: "<<key << (int)(key[key.length()-1])<< std::endl;
 	}
 	else if (command == "del") {
 		if (value == "")dict.Remove(key);
 		else dict.Remove(key, value);
-		//std::cout<< "Removed succesfully"<<std::endl
+		//std::cout << "Removed succesfully" << std::endl;
 	}
 	else if (command == "find") {
-		if (dict.innerMap->count(key) == 0)return;
+		//std::cout << "finding key: "<<key << std::endl;
+		//std::cout << dict.Count(key) << key << (int)(key[key.length() - 1]) << std::endl;
+		if (dict.Count(key) == 0)return;
 		dictValue values = dict.Find(key);
 		if (values.size() == 0) {
 			//std::cout << "There is no such key";
@@ -115,7 +122,7 @@ void ParseCommand(MyMap& dict, const std::string& buffer) {
 	}
 	else if (command == "prefix") {
 		bool isprefixkey;
-		if (dict.innerMap->count(key) == 0) {
+		if (dict.Count(key) == 0) {
 			isprefixkey = false;
 			dict.Add(key, "");
 		}
@@ -127,11 +134,11 @@ void ParseCommand(MyMap& dict, const std::string& buffer) {
 			iter++;
 			dict.Remove(key);
 		}
-		if (iter == dict.innerMap->end())return;
+		if (iter == dict.innerMap.end())return;
 		while (true) {
 			if (Isprefix(iter->first, key)) {
 				std::cout << iter->first << ": ";
-				for (std::string v : iter->second)
+				for (const std::string& v : iter->second)
 				{
 					std::cout << v << ' ';
 				}
@@ -146,9 +153,24 @@ void ParseCommand(MyMap& dict, const std::string& buffer) {
 int main()
 {
 	MyMap Dict;
+	std::string cmd, k, v;
+	char command[7], key[101], value[101];
+	int retVal;
 	std::string buffer;
-	while (std::getline(std::cin, buffer)) {
-		ParseCommand(Dict, buffer);
+	while (std::getline(std::cin, buffer))
+	{
+		//std::cout << buffer << std::endl;
+		retVal = sscanf(buffer.c_str(),"%6s %100s %100s", command, key, value);
+		if (retVal == EOF)continue;
+		cmd = command;
+		k = key;
+		v = value;
+		if (retVal == 2)v = "";
+		transform(cmd.begin(), cmd.end(), cmd.begin(), ::tolower);
+		transform(k.begin(), k.end(), k.begin(), ::tolower);
+		transform(v.begin(), v.end(), v.begin(), ::tolower);
+		ParseCommand(Dict, cmd, k, v);
 	}
+	
 
 }
