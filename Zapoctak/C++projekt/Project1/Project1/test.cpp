e7bc6c9c239bc72998544ae9de8c933a8b61eb80
LordofMadness <zemekoulak@gmail.com>
2024-03-29 16:47:49 +0100

    testing of plugins and editor scripts

--- Zapoctak/C++projekt/Project1/Project1/test.cpp
+++ Zapoctak/C++projekt/Project1/Project1/test.cpp
@@ -6,7 +6,7 @@
 using namespace godot;
 
 void GDExample::_bind_methods() {
-	ClassDB::bind_method(D_METHOD(NAMEOF(get_exportproperty)), &GDExample::get_exportporperty);
+	ClassDB::bind_method(D_METHOD(NAMEOF(get_exportproperty)), &GDExample::get_exportproperty);
 	ClassDB::bind_method(D_METHOD(NAMEOF(set_exportproperty), "value"), &GDExample::set_exportproperty);
 	ClassDB::add_property("GDExample", PropertyInfo(Variant::FLOAT, "ExportProperty"), NAMEOF(set_exportproperty), NAMEOF(get_exportproperty));
 	ClassDB::bind_method(D_METHOD(NAMEOF(SummonMadness)), &GDExample::SumoonMadness);
@@ -21,7 +21,7 @@ GDExample::GDExample() {
 GDExample::~GDExample() {
 	// Add your cleanup here.
 }
-void godot::GDExample::SumoonMadness()
+void godot::GDExample::SumoonMadness() const
 {
 	std::cout << "Hello World";
 }
@@ -29,7 +29,7 @@ void godot::GDExample::set_exportproperty(const double val)
 {
 	exportproperty = val;
 }
-double godot::GDExample::get_exportporperty()
+double godot::GDExample::get_exportproperty() const
 {
 	return exportproperty;
 }
@@ -39,4 +39,41 @@ void GDExample::_process(double delta) {
 	Vector2 new_position = Vector2(10.0 + (10.0 * sin(time_passed * 2.0)), 10.0 + (10.0 * cos(time_passed * 1.5)));
 
 	set_position(new_position);
+}
+
+void godot::GDEditorTest::_notification(int what)
+{
+}
+
+void godot::GDEditorTest::_bind_methods()
+{
+	ClassDB::bind_method(D_METHOD(NAMEOF(SummonMadnessAGAIN)), &GDEditorTest::SummonMadnessAGAIN);
+}
+
+void godot::GDEditorTest::SummonMadnessAGAIN()
+{
+	std::cout << "MADNESS WILL CONSUME YOU!";
+}
+
+void godot::GDEditorTest::_edit(Object* p_object)
+{
+}
+
+bool godot::GDEditorTest::_handles(Object* p_object) const
+{
+	return false;
+}
+
+void godot::GDEditorTest::_make_visible(bool p_visible)
+{
+}
+
+void godot::GDEditorTest::_enter_tree()
+{
+	//__super::_enter_tree(); It seems enter_tree truly does nothign (haven't found any implementation other than mine anywhere)
+}
+
+void godot::GDEditorScriptTest::_bind_methods()
+{
+	ClassDB::bind_method(D_METHOD(NAMEOF(SummoningMadnessForAllEternity)), &GDEditorScriptTest::SummoningMadnessForAllEternity);
 }
--- Zapoctak/C++projekt/Project1/Project1/test.hpp
+++ Zapoctak/C++projekt/Project1/Project1/test.hpp
@@ -2,11 +2,14 @@
 #define GDEXAMPLE_H
 
 #include <godot_cpp/classes/sprite2d.hpp>
+#include <godot_cpp/classes/editor_plugin.hpp>
+#include <godot_cpp/classes/editor_interface.hpp>
+#include <godot_cpp/classes/editor_script.hpp>
 #define NAMEOF(name) #name
 
 namespace godot {
 
-	class GDExample : public Sprite2D {
+	class GDExample : public Sprite2D {//EXAMPLE Node
 		GDCLASS(GDExample, Sprite2D)
 
 	private:
@@ -19,10 +22,46 @@ namespace godot {
 	public:
 		GDExample();
 		~GDExample();
-		void SumoonMadness();
+		void SumoonMadness() const;
 		void set_exportproperty(const double val);
-		double get_exportporperty();
+		double get_exportproperty() const;
 		void _process(double delta) override;
+	};
+
+	class GDEditorTest : public EditorPlugin {//EXAMPLE PLUGIN
+		GDCLASS(GDEditorTest, EditorPlugin)
+
+	private:
+	protected:
+		void _notification(int what);
+	public:
+		static void _bind_methods();
+		GDEditorTest() {}
+		~GDEditorTest() {}
+		void SummonMadnessAGAIN();
+		virtual String _get_plugin_name() const override { return "Test"; }
+		virtual bool _has_main_screen() const override { return false; }
+		virtual void _edit(Object* p_object) override;
+		virtual bool _handles(Object* p_object) const override;
+		virtual void _make_visible(bool p_visible) override;
+		virtual void _enter_tree()override;
+
+	};
+
+	class GDEditorScriptTest : public EditorScript {//EXAMPLE EditorScript
+		GDCLASS(GDEditorScriptTest, EditorScript)
+	protected:
+	public:
+		static void _bind_methods();
+		GDEditorScriptTest(){}
+		~GDEditorScriptTest() {}
+		void SummoningMadnessForAllEternity() {
+			std::cout << "BOW DOWN BEFORE THE GOD OF DEATH, thot!";
+		}
+		virtual void _run()override {
+			SummoningMadnessForAllEternity();
+		}
+
 	};
 
 }
